ab69457bf9df69dca9a4aee24f3c5ded7c166dc3
proba <varga9316@gmail.com>
2018-11-08 22:37:35 +0100

    apk built.

--- Modules/Map.js
+++ Modules/Map.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {AppRegistry, Platform, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, Platform, StyleSheet, Text, View, Dimensions, PermissionsAndroid} from 'react-native';
 import MapView,{ Marker} from 'react-native-maps';
 
 export default class MyMap extends Component{
@@ -20,13 +20,19 @@ export default class MyMap extends Component{
                     title: 'Singapore EXPO',
                     description: 'Singapore EXPO Convention and Exhibition Center '}],
             myPosition: '',
-            latitude: 0,
-            longitude: 0,
-            prevLatLong: {}
-        }
+            prevLatLong: {},
+            permissionState: false,
+            latitude: null,
+            longitude: null,
+            error: null
+        };
     }
 
-    componentDidMount()
+    componentDidMount() {
+        Platform.OS === 'android' && Platform.Version >= 23 ? this.requestMapPermission() : this.requestMap()
+    }
+
+    /*componentDidMount()
     {
         navigator.geolocation.getCurrentPosition(position =>{
 
@@ -59,16 +65,108 @@ export default class MyMap extends Component{
             { timeout: 20000, maximumAge: 1000}
         );
 
+    }*/
+
+    async requestMapPermission() {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('Granted');
+                this.watchId = navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        console.log('Position is watched');
+                        this.setState({
+                            permissionState: true,
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude,
+                            error: null,
+                        });
+                    },
+                    (error) => this.setState({error: error.message}),
+                    {enableHighAccuracy: false, timeout: 20000, maximumAge: 1000},
+                );
+
+            } else {
+                console.log('not Granted');
+                this.setState({
+                    permissionState: false,
+                });
+            }
+        } catch (err) {
+            console.warn(err)
+        }
     }
 
-    getMapRegion = () =>({
+    requestMap() {
+        this.watchId = navigator.geolocation.watchPosition(
+            (position) => {
+                this.setState({
+                    permissionState: true,
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null,
+                });
+            },
+            (error) => this.setState({error: error.message, permissionState: false,}),
+            {enableHighAccuracy: false, timeout: 20000, maximumAge: 1000},
+        );
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchID);
+    }
+
+    /*getMapRegion = () =>({
         latitude: this.state.latitude,
         longitude: this.state.longitude,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
-    });
+    });*/
     render() {
-        if (this.state.myPosition === '') {
+        var {height, width} = Dimensions.get('window');
+        return (
+            <View style={styles_map.container}>
+                {
+                    this.state.permissionState === true ?
+                        <MapView
+                            showsUsetLocation={true}
+                            followsUserLocation={true}
+                            loadingEnabled={true}
+                            style={styles_map.map}
+                            region={{
+                                latitude: this.state.latitude,
+                                longitude: this.state.longitude,
+                                latitudeDelta: 0.0922,
+                                longitudeDelta: 0.0421
+                            }}>
+                            <MapView.Marker
+                                coordinate={{
+                                    latitude: (this.state.latitude + 0.00000),
+                                    longitude: (this.state.longitude + 0.00000),
+                                }}
+                                pinColor={"green"}
+                                title = "My position"/>
+                            {this.state.markers.map(marker => (
+                                <MapView.Marker
+                                    coordinate={marker.latlng}
+                                    title={marker.title}
+                                    description={marker.description}
+                                />
+                            ))}
+                        </MapView>
+                        :
+                        <View style={styles_map.loading_text}>
+                            <Text>Loading</Text>
+
+                        </View>
+
+
+                }
+
+            </View>
+        );
+        /*if (this.state.myPosition === '') {
             return(
                 <View style={styles_map.loading_text}>
                     <Text>Loading GPS location</Text>
@@ -101,7 +199,7 @@ export default class MyMap extends Component{
                     </MapView>
                 </View>
             )
-        }
+        }*/
     }
 }
 const styles_map = StyleSheet.create({
